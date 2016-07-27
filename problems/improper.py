2534ae5914451ccdd6c308ab5fb21554b016398a
Dan F-M <danfm@nyu.edu>
2016-07-27 10:03:09 -0700

    Adding problems for likelihood section

---
+++ problems/improper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import numpy as np
+import matplotlib.pyplot as pl
+
+from plot_setup import setup, SQUARE_FIGSIZE, COLORS, savefig
+
+setup()  # initialize the plotting styles
+np.random.seed(42)
+
+
+x = 0.0
+chain = np.empty(1e6)
+for step in range(len(chain)):
+    x += np.random.randn()
+    chain[step] = x
+
+# Thin the chain for plotting purposes.
+chain = chain[::1000]
+mn, mx = np.min(chain), np.max(chain)
+mid = 0.5 * (mn + mx)
+rng = 0.5 * (mx - mn) * 1.1
+
+fig, ax = pl.subplots(1, 1, figsize=SQUARE_FIGSIZE)
+ax.plot(np.arange(len(chain)) * 1e-2, chain, color=COLORS["DATA"])
+ax.yaxis.set_major_locator(pl.MaxNLocator(5))
+ax.set_xlabel("mcmc step [$\\times 10^5$]")
+ax.set_ylabel("$x$")
+ax.set_ylim(mid-rng, mid+rng)
+
+savefig(fig, "improper.pdf")
---
+++ problems/logprior.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import numpy as np
+import matplotlib.pyplot as pl
+
+from plot_setup import setup, SQUARE_FIGSIZE, COLORS, savefig
+
+setup()  # initialize the plotting styles
+np.random.seed(42)
+
+
+def log_p(x, mean=2.0, variance=2.0):
+    return -0.5 * (x - mean)**2 / variance - 0.5*np.log(2*np.pi*variance)
+
+
+log_x = 0.0
+lp = log_p(log_x)
+chain = np.empty(2e6)
+for step in range(len(chain)):
+    log_x_prime = log_x + np.random.randn()
+    lp_prime = log_p(log_x_prime)
+    if np.random.rand() <= np.exp(lp_prime - lp):
+        log_x = log_x_prime
+        lp = lp_prime
+    chain[step] = np.exp(log_x)
+
+
+fig, ax = pl.subplots(1, 1, figsize=SQUARE_FIGSIZE)
+x = np.linspace(0, 20, 5000)
+ax.plot(x, np.exp(log_p(np.log(x))) / x, color=COLORS["MODEL_1"])
+ax.hist(chain, 10000, histtype="step", color=COLORS["DATA"], normed=True)
+ax.set_xlim(x.min(), x.max())
+ax.yaxis.set_major_locator(pl.MaxNLocator(4))
+ax.set_xlabel("$x$")
+ax.set_ylabel("$p(x)$")
+
+savefig(fig, "logprior.pdf")
