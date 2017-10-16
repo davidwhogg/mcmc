3a5f6378ac5e1786678481d5fb564624ec836048
Dan F-M <danfm@nyu.edu>
2017-10-16 07:19:40 -0400

    paper name

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -9,7 +9,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan pdf
   git rm -rf .
-  git add -f ms.pdf
+  git add -f mcmc.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://dfm:$GITHUB_API_KEY@github.com/davidwhogg/mcmc pdf
 fi
